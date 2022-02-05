b081dba90d42800d3cec17c8a9cc1086b1d64a25
Arthur Geel <geelaj01@gmail.com>
2022-02-05 12:06:30 +0100

    Set up custom Tailwind CSS branding

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="max-w-lg mx-auto">
+      <h1 className="text-3xl font-bold text-gray-900">
+        Hello world!
+      </h1>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Space Grotesk', 'Inter', 'sans-serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#FFFFFF',
+      'gray-100': '#F4F4F4',
+      'gray-400': '#A699A5',
+      'gray-500': '#8F7F8E',
+      'gray-600': '#634E63',
+      'gray-900': '#20011F',
+      'amber-100': '#EDE8E1',
+      'amber-200': '#CBC8C3',
+      'amber-500': '#A07F4D',
+    },
     extend: {},
   },
   plugins: [],
