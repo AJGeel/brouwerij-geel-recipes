b5fe065d9219b9798e436945cd3587dcb3242ace
Arthur Geel <geelaj01@gmail.com>
2022-02-06 17:38:39 +0100

    Bugfix: crossOrigin

--- pages/recepten/[slug].js
+++ pages/recepten/[slug].js
@@ -83,7 +83,7 @@ export default function RecipePage({ frontmatter, content }) {
         <>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
             </Head>
 
