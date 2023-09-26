97b3355ed88af3f422742b6ef618bb1b249fc74a
Arthur Geel <ageel@enrise.com>
2023-09-26 23:40:24 +0200

    Improve tag Header design

--- src/app/tag/[slug]/Header.tsx
+++ src/app/tag/[slug]/Header.tsx
@@ -1,17 +1,18 @@
 import HeaderLink from "./HeaderLink";
 
-export const Header = ({ title }: { title: string }) => {
-  return (
-    <div className="mt-9 flex items-center justify-between">
-      <HeaderLink href="/" isLeft={true} title="Alle recepten" />
-
-      <div className="flex flex-col items-center">
-        <p className="text-gray-500">Je bekijkt:</p>
-        <h1 className="text-3xl font-bold text-gray-900 text-center">
-          #{title}
-        </h1>
-      </div>
-      <HeaderLink href="/tag" isLeft={false} title="Alle tags" />
-    </div>
-  );
+type Props = {
+  title: string;
 };
+
+export const Header = ({ title }: Props) => (
+  <div className="mt-9 flex flex-row-reverse md:flex-row items-center justify-between">
+    <HeaderLink href="/" isLeft={true} title="Alle recepten" />
+    <div className="flex flex-col items-start md:items-center">
+      <p className="text-gray-500">Je bekijkt:</p>
+      <h1 className="text-xl md:text-3xl font-bold text-gray-900 text-center truncate">
+        #{title}
+      </h1>
+    </div>
+    <HeaderLink href="/tag" isLeft={false} title="Alle tags" />
+  </div>
+);
