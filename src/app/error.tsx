921af708b5361838b1cbde99327b5ec19da5e6b3
Arthur Geel <ageel@enrise.com>
2023-09-26 23:23:59 +0200

    Add generic error message

---
+++ src/app/error.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+type Props = {
+  error: Error & { digest?: string };
+  reset: () => void;
+};
+
+const Error = ({ error, reset }: Props) => (
+  <div className="flex items-center flex-grow justify-center">
+    <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-neutral-200 bg-white p-8 md:p-12">
+      <h2 className="text-xl font-bold">Oh jee!</h2>
+      <p className="my-2">
+        Deze pagina kan niet getoond worden. Mogelijk heeft iemand bier gemorst
+        over de server...
+      </p>
+      <p className="text-sm my-2">Referentie: {error.digest}.</p>
+      <button
+        className="mx-auto mt-4 flex w-full items-center justify-center rounded-full bg-amber-100 p-4 tracking-wide text-gray-900 hover:bg-amber-200 duration-150"
+        onClick={() => reset()}
+      >
+        Probeer het opnieuw
+      </button>
+    </div>
+  </div>
+);
+
+export default Error;
